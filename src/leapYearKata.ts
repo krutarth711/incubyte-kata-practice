48c6e4119babbe433c5f90c0fec1eb3381f4f6c4
krutarth711 <38184361+krutarth711@users.noreply.github.com>
2025-02-19 22:11:52 +0530

    Add logic for years not divisible by 4 case

--- src/leapYearKata.ts
+++ src/leapYearKata.ts
@@ -1,5 +1,5 @@
 const isLeapYear = (year = 0): boolean => {
-    if (!year || (year % 100 === 0 && year % 400 !== 0)) {
+    if (!year || (year % 100 === 0 && year % 400 !== 0) || year % 4 !== 0) {
         return false;
     }
     return true;
