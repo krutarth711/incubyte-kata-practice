e7834d4522b0cf8efb710bbfa14e45461b218035
krutarth711 <38184361+krutarth711@users.noreply.github.com>
2025-02-19 22:08:49 +0530

    Add test case for a year divisible by 4 but not by 100

--- test/leapYearKata.test.ts
+++ test/leapYearKata.test.ts
@@ -12,3 +12,8 @@ it('Should return false for a year divisible by 100 but not by 400', () => {
    expect(isLeapYear(1900)).toBe(false);
    expect(isLeapYear(1800)).toBe(false);
 });
+
+it('Should return true for a year divisible by 4 but not by 100', () => {
+   expect(isLeapYear(2004)).toBe(true);
+   expect(isLeapYear(2008)).toBe(true);
+});
