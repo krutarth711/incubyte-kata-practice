075ff4d08f7b0b7ca523d2d549e8e3a5e0ce80b6
krutarth711 <38184361+krutarth711@users.noreply.github.com>
2025-02-19 22:33:21 +0530

    Correct test case for year 0

--- test/leapYearKata.test.ts
+++ test/leapYearKata.test.ts
@@ -1,8 +1,12 @@
 import isLeapYear from "../src/leapYearKata";
 
-it('Should return false for null or zero value', () => {
-   expect(isLeapYear(0)).toBe(false);
+it('Should return false for null value', () => {
+   expect(isLeapYear()).toBe(false);
 });
+
+it('Should return false for 0', () => {
+    expect(isLeapYear(0)).toBe(true);
+ });
 
 it('Should return true for a year divisible by 400', () => {
    expect(isLeapYear(2000)).toBe(true);
