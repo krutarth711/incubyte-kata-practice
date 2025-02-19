06406e76e662e19e44a3d0875c855c3994e4a898
krutarth711 <38184361+krutarth711@users.noreply.github.com>
2025-02-19 21:57:02 +0530

    Add test case for year divisible by 400

--- test/leapYearKata.test.ts
+++ test/leapYearKata.test.ts
@@ -3,3 +3,7 @@ import isLeapYear from "../src/leapYearKata";
 it('Should return false for null or zero value', () => {
    expect(isLeapYear(0)).toBe(false);
 });
+
+it('Should return true for a year divisible by 400', () => {
+   expect(isLeapYear(2000)).toBe(true);
+});
